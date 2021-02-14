8c81360c03065362f01eeac9dd11354bcc3a9feb
Yogesh Dhanapal <geoyogesh@gmail.com>
2021-02-14 10:31:58 -0500

    included deckgl layer

--- crash_analysis/src/pages/components/map-canvas.js
+++ crash_analysis/src/pages/components/map-canvas.js
@@ -1,5 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import React, { useState, useEffect, useRef } from 'react';
+import {ScatterplotLayer} from '@deck.gl/layers';
+import {MapboxLayer} from '@deck.gl/mapbox';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
@@ -20,6 +22,23 @@ const MapCanvas = () => {
             map.on('move', () => {
                 console.log(map.getCenter());
               });
+
+              const myDeckLayer = new MapboxLayer({
+                id: 'my-scatterplot',
+                type: ScatterplotLayer,
+                data: [
+                  {position: [-77.79218893867733, 37.82963522630058], size: 1000}
+                ],
+                getPosition: d => d.position,
+                getRadius: d => d.size,
+                getColor: [255, 0, 0]
+              });
+            
+              
+              map.on('load', () => {
+                map.addLayer(myDeckLayer);
+              });
+
             // Clean up on unmount
             return () => map.remove();
       }, []);
