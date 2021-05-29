a25d8fcb550a36d87825a95c3f13f5ead31d0c55
Yogesh Dhanapal <geoyogesh@gmail.com>
2021-05-29 01:01:54 -0400

    working on data preperation

--- data_job/scripts/setup.md
+++ data_job/scripts/setup.md
@@ -7,3 +7,35 @@ conda install python=3 geopandas pygeos pyarrow fastparquet feather-format -y
 ```
 
 /usr/local/anaconda3/envs/crash_analysis_scripts
+
+ogr2ogr -f GeoJSON Crash.geojson Crash.gdb --debug on -progress
+ogr2ogr -f MVT Crash_VectorTile Crash.geojson -dsco MAXZOOM=10 -dsco COMPRESS=NO --debug on -progress
+
+ogr2ogr -f MVT Crash_VectorTile Crash.geojson -dsco MAXZOOM=10 -dsco COMPRESS=NO --debug on -progress  -skipfailures
+
+ogr2ogr -f MVT Crash_VectorTile error.geojson -dsco MAXZOOM=10 -dsco COMPRESS=NO --debug on -progress  -skipfailures
+
+ogr2ogr -f SQLite Crash.sqlite Crash.geojson  -dsco SPATIALITE=YES --debug on -progress -skipfailures -lco ENCODING=UTF-8
+
+---
+ogr2ogr -f GeoJSON Crash.geojson Crash.gdb --debug on -progress -skipfailures -lco ENCODING=UTF-8
+
+ogr2ogr -f GPKG Crash.gpkg Crash.gdb --debug on -progress -skipfailures -lco ENCODING=UTF-8
+
+ogr2ogr -f GPKG Crash.gpkg Crash.gdb --debug on -progress -skipfailures -oo ENCODING=UTF-8
+
+ERROR 1: At line 808531, character 4: Unexpected state
+ERROR 4: Failed to read GeoJSON data
+FAILURE:
+
+-lco ENCODING=UTF-8
+
+ogr2ogr -f GeoJSON Crash.geojson Crash.gdb --debug on -progress -skipfailures -t_srs EPSG:4326
+tippecanoe -zg -o out.mbtiles --drop-densest-as-needed Crash.geojson --force
+
+ogr2ogr -f MVT Crash_VectorTile Crash.geojson -dsco MAXZOOM=10 -dsco COMPRESS=NO --debug on -progress 
+
+ogr2ogr -f SQLite Crash.sqlite Crash.geojson  -dsco SPATIALITE=YES --debug on -progress -skipfailures -lco SPATIAL_INDEX=NO -t_srs EPSG:3857  --config NUM_THREADS ALL_CPUS --config OGR_SQLITE_CACHE 1024 --config OGR_SQLITE_SYNCHRONOUS OFF
+
+ogr2ogr -f GPKG Crash.gpkg Crash.geojson --debug on -progress -skipfailures -lco SPATIAL_INDEX=NO -t_srs EPSG:3857 --config NUM_THREADS ALL_CPUS --config OGR_SQLITE_CACHE 1024 --config OGR_SQLITE_SYNCHRONOUS OFF
+
--- data_job/scripts/write_data.py
+++ data_job/scripts/write_data.py
@@ -1,35 +1,35 @@
 import geopandas as gp
 import warnings; warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
 
-base_path = r'/Users/yogeshd/Desktop/timmons/crash-analysis/data_job/scripts/'
+base_path = r'/mnt/c/Temp/GitHub/crash-analysis/data_job/scripts/'
 
 def main():
     
     layers = [
-        ('Census_Urban_Areas', 'VDOT_SMOOTHED_URBAN_AREAS', 'geojson'),
-        ('MPO_Study_Areas', 'MPO_STUDY_AREAS', 'geojson'),
-        ('Planning_District_Commissions', 'PLANNING_DISTRICT_COMMISSION', 'geojson'),
-        ('VDOT_Area_HQ', 'VDOT_Area_Headquarters', 'geojson'),
-        ('VDOT_Districts', 'VDOT_DISTRICTS', 'geojson'),
-        #('Virginia_County_Jurisdictions', 'VA_COUNTY', 'geojson'),
-        #('Virginia_Tax_Parcels', 'VA_Parcels', 'geojson'),
-        #('Virginia_Town_Jurisdictions', 'VA_TOWN', 'geojson'),
-        ('Crash', 'Crash', 'fgdb')
+        #('Census_Urban_Areas', 'VDOT_SMOOTHED_URBAN_AREAS', 'geojson', True),
+        #('MPO_Study_Areas', 'MPO_STUDY_AREAS', 'geojson', True),
+        #('Planning_District_Commissions', 'PLANNING_DISTRICT_COMMISSION', 'geojson', True),
+        #('VDOT_Area_HQ', 'VDOT_Area_Headquarters', 'geojson', True),
+        #('VDOT_Districts', 'VDOT_DISTRICTS', 'geojson', True),
+        #('Virginia_County_Jurisdictions', 'VA_COUNTY', 'geojson', True),
+        #('Virginia_Tax_Parcels', 'VA_Parcels', 'geojson', True),
+        #('Virginia_Town_Jurisdictions', 'VA_TOWN', 'geojson', True),
+        ('Crash', 'Crash', 'geojson', False)
     ]
 
     for layer in layers:
-        layer_name, layer_file_name, file_type = layer
+        layer_name, layer_file_name, file_type, is_archive = layer
         print ('processing ', layer_name, layer_file_name)
-        if file_type == 'fgdb':
-            states = gp.read_file(f'zip://{base_path}/source/{layer_name}.zip!{layer_file_name}.gdb')
+        if is_archive:
+            states = gp.read_file(f'zip://{base_path}/source/{layer_name}.zip!{layer_file_name}.{file_type}')
         else:
-            states = gp.read_file(f'zip://{base_path}/source/{layer_name}.zip!{layer_file_name}.geojson')
+            states = gp.read_file(f'{base_path}/source/{layer_name}.{file_type}')
         #states["wkb"] = states.geometry.apply(lambda g: g.wkb if g is not None else None)
         #states = states.drop(columns=["geometry"])
-        states.to_parquet(f'{base_path}/output/{layer_name}.parquet') 
-        states.to_parquet(f'{base_path}/output/{layer_name}_index.parquet', index=True) 
-        states.to_feather(f'{base_path}/output/{layer_name}.feather')
-        states.to_feather(f'{base_path}/output/{layer_name}_index.feather', index=True)
+        #states.to_parquet(f'{base_path}/output/{layer_name}.parquet') 
+        #states.to_parquet(f'{base_path}/output/{layer_name}_index.parquet', index=True) 
+        #states.to_feather(f'{base_path}/output/{layer_name}.feather')
+        #states.to_feather(f'{base_path}/output/{layer_name}_index.feather', index=True)
 
     print ('completed...')
 
